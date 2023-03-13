f95eb15120386d58dc4c622f0c769d799381679d
chawincode <chawin.suw@dome.tu.ac.th>
2023-03-14 03:36:51 +0700

    complete login/register

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { setCookie } from 'cookies-next';
+import { useRouter } from 'next/router'
 
 const index = () => {
+    const router = useRouter();
+
+    //user handle
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+    //Fetch data
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          "email": email,
+          "password" : pass,
+        })
+      };
+
+    const postLogin = async () => {
+        await fetch('http://localhost:8080/auth/login', requestOptions)
+          .then(response => {
+            response.json()
+                .then(data => {
+                  setCookie('token', data.token);
+                  setCookie('firstname', data.first_name);
+                  setCookie('lastname', data.last_name);
+                  setCookie('nickname', data.nick_name);
+                  setCookie('phone', data.phone);
+                  setCookie('stdID', data.student_id)
+                  setCookie('id', data.user_id);
+                  console.log(data)
+                  console.log(data.token)
+                  console.log(data.first_name)
+                  console.log(data.last_name)
+                  console.log(data.nick_name)
+                  console.log(data.phone)
+                  console.log(data.classrooms)
+                  console.log("Email = " + email)
+                  console.log("Password = " + pass)
+                  router.push('/')
+                });
+        })
+        .catch (error => {
+          if (error.message == 'Error: You need to specify name or key when calling navigate with an object as the argument. See https://reactnavigation.org/docs/navigation-actions#navigate for usage.') {
+          }
+          console.error(error);
+        }) 
+      }
+    //function loading
     function simulateNetworkRequest() {
         return new Promise((resolve) => setTimeout(resolve, 2000));
     }
@@ -16,9 +65,11 @@ const index = () => {
     }
     }, [isLoading]);
 
-    const handleClick = () => {setLoading(true)};
+    const handleClick = () => {setLoading(true), postLogin()};
 
     return (
+        <>
+        
         <main className='bg-18191B w-screen h-screen'>
             <div className='flex h-screen'>
                 <form className="m-auto md:w-2/6">
@@ -29,14 +80,37 @@ const index = () => {
                         </p>
                     </div>
 
-                    <div className="mb-1">
+                    {/* <div className="mb-1">
                         <label for="email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" id="email" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Email" required />
+                        <input type="email" id="email" className="bg-white outline-offset-0 outline-offset-2 outline-B66D43 text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-18191B dark:placeholder-gray-400 dark:text-18191B" placeholder="Email" required />
                     </div>
                     <div className="mb-6">
                         <label for="email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                        <input type="password" id="password" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Password" required />
+                        <input type="password" id="password" className="bg-white outline-offset-0 outline-offset-2 outline-B66D43 text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Password" required />
+                    </div> */}
+                    <div className="mb-1">
+                        <label for="email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            className="bg-white outline-none text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-18191B dark:placeholder-gray-400 dark:text-18191B" 
+                            placeholder="Email" 
+                            required 
+                            onChange={e => setEmail(e.target.value)}
+                        />
                     </div>
+                    <div className="mb-6">
+                        <label for="email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            className="bg-white outline-none text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                            placeholder="Password" 
+                            required 
+                            onChange={e => setPass(e.target.value)}
+                        />
+                    </div>
+
                     <div className="flex justify-center mb-6">
                         <div className="flex items-center md:h-7 h-5">
                             <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
@@ -58,6 +132,7 @@ const index = () => {
                 </form>
             </div>
         </main>
+        </>
   )
 }
 
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,7 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router'
+import { setCookie } from 'cookies-next';
 
 const index = () => {
+    const router = useRouter();
+
+    const [first, setFirst] = useState("");
+    const [last, setLast] = useState("");
+    const [nick, setNick] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [phone, setPhone] = useState("");
+    const [stdID, setStdID] = useState("");
+    const type = useState("USER");
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "first_name": first,
+        "last_name": last,
+        "nick_name" : nick,
+        "email": email,
+        "password" : pass,
+        "phone": phone,
+        "student_id": stdID,
+        "User_type": type
+      })
+    };
+  
+    const postSignIN = async () => {
+      await fetch('http://localhost:8080/auth/signup', requestOptions)
+        .then(response => {
+          response.json()
+              .then(data => {
+                console.log(data.token)
+                setCookie('token', data.token);
+                console.log("First Name = " + first)
+                console.log("Last Name = " + last)
+                console.log("Nick Name = " + nick)
+                console.log("Email = " + email)
+                console.log("Password = " + pass)
+                console.log("Phone = " + phone)
+                console.log("Type = " + type)
+                router.push('/')
+              });
+      })
+      .catch (error => {
+        if (error.message == 'Error: You need to specify name or key when calling navigate with an object as the argument. See https://reactnavigation.org/docs/navigation-actions#navigate for usage.') {
+        }
+        console.error(error);
+      }) 
+    }
+
     function simulateNetworkRequest() {
         return new Promise((resolve) => setTimeout(resolve, 2000));
     }
@@ -16,9 +67,11 @@ const index = () => {
     }
     }, [isLoading]);
 
-    const handleClick = () => {setLoading(true)};
+    const handleClick = () => {setLoading(true), postSignIN()};
 
     return (
+        <>
+        
         <main className='bg-18191B w-screen h-screen'>
             <div className='flex h-screen'>
                 <form className="m-auto w-3/4">
@@ -32,38 +85,93 @@ const index = () => {
                         <div>
                             <div className="mb-1">
                                 <label for="phone" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your phone</label>
-                                <input type="number" id="phone" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Phone" required />
+                                <input 
+                                    type="number" 
+                                    id="phone" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Phone" 
+                                    required 
+                                    onChange={e => setPhone(e.target.value)}
+                                />
                             </div>
                             <div className="mb-1">
                                 <label for="email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" id="email" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Email" required />
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Email" 
+                                    required 
+                                    onChange={e => setEmail(e.target.value)}
+                                />
                             </div>
                             <div className="mb-1">
                                 <label for="password" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                                <input type="password" id="password" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Password" required />
+                                <input 
+                                    type="password" 
+                                    id="password" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Password" 
+                                    required 
+                                    onChange={e => setPass(e.target.value)}
+                                />
                             </div>                            
                             <div className="mb-1">
                                 <label for="confirm_email" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                <input type="password" id="confirm_email" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Confirm Password" required />
+                                <input 
+                                    type="password" 
+                                    id="confirm_email" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Confirm Password" 
+                                    required 
+                                />
                             </div>
                         </div>
                         
                         <div>
                             <div className="mb-1">
                                 <label for="first_name" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your first name</label>
-                                <input type="text" id="first_name" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="First Name" required />
+                                <input 
+                                    type="text" 
+                                    id="first_name" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="First Name" 
+                                    required 
+                                    onChange={e => setFirst(e.target.value)}
+                                />
                             </div>
                             <div className="mb-1">
                                 <label for="last_name" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your last name</label>
-                                <input type="text" id="last_name" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Last Name" required />
+                                <input 
+                                    type="text" 
+                                    id="last_name" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Last Name" 
+                                    required 
+                                    onChange={e => setLast(e.target.value)}
+                                />
                             </div>
                             <div className="mb-1">
                                 <label for="nick_name" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your nick name</label>
-                                <input type="text" id="nick_name" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Nick Name" required />
+                                <input 
+                                    type="text" 
+                                    id="nick_name" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Nick Name" 
+                                    required 
+                                    onChange={e => setNick(e.target.value)}
+                                />
                             </div>
                             <div className="mb-1">
                                 <label for="stdID" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Your student ID</label>
-                                <input type="number" id="stdID" className="bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" placeholder="Student ID" required />
+                                <input 
+                                    type="number" 
+                                    id="stdID" 
+                                    className="outline-none bg-white text-18191B text-sm rounded-lg block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-18191B" 
+                                    placeholder="Student ID" 
+                                    required
+                                    onChange={e => setStdID(e.target.value)}
+                                />
                             </div>
                         </div>
 
@@ -83,6 +191,7 @@ const index = () => {
                 </form>
             </div>
         </main>
+        </>
   )
 }
 
