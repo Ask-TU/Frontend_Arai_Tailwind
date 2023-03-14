886afad8c21fc361eeabdc006fdc56f3f9e2e1f8
Tanawat Limthongkul <113848137+Tanawatlim@users.noreply.github.com>
2023-03-14 17:10:24 +0700

    Update Home-page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,38 @@
-import { Header, Footer } from '@/components/layout/Layout'
-import Navbar from '@/components/layout/Navbar'
-import React from 'react'
+import Search from "@/components/common/Search";
+import { Header, Footer } from "@/components/layout/Layout";
+import Navbar from "@/components/layout/Navbar";
+import React from "react";
 
 const index = () => {
   return (
     <>
-    <Header />
-    <div className='grid grid-cols-5 h-screen'>
-      <div>
-        <Navbar />
-      </div>
-      <div className='col-span-4'>
-        <p className='text-white'>Content</p>
-      </div>
-    </div>
-    <Footer />
-    </>
-  )
-}
+      <Header />
+      <div className="mt-5 h-screen xl:grid grid-cols-5">
+        <div className="col-span-1 max-xl:hidden">
+          <Navbar />
+        </div>
 
-export default index
+        <div className="col-span-5 xl:col-span-4">
+          <div className="m-auto max-w-2xl">
+            <Search />
+
+            <div className="flex justify-center items-center w-full h-60 border-2 my-5 rounded-md text-white">
+              Block post
+            </div>
+
+            <div className="flex justify-center items-center w-full h-60 border-2 my-5 rounded-md text-white">
+              Block post
+            </div>
+
+            <div className="flex justify-center items-center w-full h-60 border-2 my-5 rounded-md text-white">
+              Block post
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default index;
