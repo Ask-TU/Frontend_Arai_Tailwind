bb5fe27ebf2e3c81f6c765e82fa11bbdc3d093ec
thanks <phuwich.winyutrakul@gmail.com>
2023-03-16 23:15:44 +0700

    Update text in line

--- src/pages/notification/index.js
+++ src/pages/notification/index.js
@@ -20,7 +20,7 @@ const index = () => {
             </div>
             <div class="inline-flex items-center justify-center w-full">
               <hr class="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-              <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Notification</span>
+              <span class="absolute px-3 font-medium text-white -translate-x-1/2 bg-18191B left-1/2 dark:text-white dark:bg-gray-900">Notification</span>
             </div>
               <div className='mt-5 pt-5'>
                 <Blocknoti/>
