9c0170eaa0c2be447dda50fdc5fcb9ce20901543
chawincode <chawin.suw@dome.tu.ac.th>
2023-03-19 22:08:56 +0700

    Logout Button

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,8 +5,18 @@ import ProfileImg from "../../../public/assets/Ellipse 7.png";
 import Link from "next/link";
 import { BsFillBellFill, BsPersonCircle } from "react-icons/bs";
 import { RiQuestionnaireFill } from "react-icons/ri";
+import { HiOutlineLogout } from "react-icons/hi"
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const [alert, setAlert] = useState(false);
+  const logout = () => {
+    deleteCookie('token');
+    setAlert(false);
+    router.push('/');
+  }
   return (
     <div className="z-10 flex justify-between border-b border-black bg-242527 p-4 sticky top-0">
       <div className="mx-5">
@@ -20,11 +30,35 @@ const Header = () => {
             <BsFillBellFill />
           </Link>
         </button>
-        <Link href={"/"}>
-          <button className="text-2xl text-white mx-3">
-            <BsPersonCircle />
-          </button>
-        </Link>
+        <button className="text-2xl text-white mx-3" onClick={()=>setAlert(true)}>
+          <HiOutlineLogout />
+        </button>
+        {alert && (
+          <div class="fixed inset-0 overflow-y-auto">
+            <div class="flex items-center justify-center min-h-screen">
+              <div class="fixed inset-0 bg-272727 bg-opacity-75 backdrop-blur">
+                <div id="alert-additional-content-2" class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800 relative bg-242527 text-white rounded-lg max-w-2xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 drop-shadow-lg" role="alert">
+                  <div class="flex items-center">
+                    <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Info</span>
+                    <h3 class="text-lg font-medium">Log out!</h3>
+                  </div>
+                  <div class="mt-2 mb-4 text-sm">
+                    Are you sure?
+                  </div>
+                  <div class="flex">
+                    <button type="button" class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800" data-dismiss-target="#alert-additional-content-2" aria-label="Close" onClick={()=>logout()}>
+                      Yes
+                    </button>
+                    <button type="button" class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 ml-5" data-dismiss-target="#alert-additional-content-2" aria-label="Close" onClick={()=>setAlert(false)}>
+                      No
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}   
       </div>
     </div>
   );
