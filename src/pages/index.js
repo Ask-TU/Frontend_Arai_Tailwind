c58cb0440071aa5e418c9d9bf7f24e96675aee16
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-04-13 20:57:15 +0700

    Update Indexjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,7 @@ import { hasCookie } from 'cookies-next';
 import Login from "./login";
 import Home from "./home";
 
-export default function App(){
+export default function App() {
   const token = hasCookie('token');
-  if(token){
-      return <Home/>
-    } else {
-      return <Login/>
-    }
+  return token ? <Home /> : <Login />;
 }
