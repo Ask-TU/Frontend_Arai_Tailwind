a9ac2304d8cfb41ea7adfe60e540da59a9e1ba51
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-04-23 21:20:25 +0700

    Update

---
+++ src/pages/game.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+export default function Game() {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+
+    const fetchData = async () => {
+        setLoading(true)
+        fetch('/api/hello')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                console.log(data)
+                setLoading(false)
+            })
+    }
+
+    return (
+        <div>
+            <button onClick={fetchData} className='bg-white'>Fetch Data</button>
+            {
+                data ? <p>Loading...</p> : <p>{data.class_items}</p>
+            }
+        </div>
+    )
+}
