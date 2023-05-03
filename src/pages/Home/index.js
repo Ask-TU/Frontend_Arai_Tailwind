258be7bb06a585f3e772a043e88ca1ddb694f13e
Tanawat Limthongkul <113848137+Tanawatlim@users.noreply.github.com>
2023-05-03 21:27:26 +0700

    Hot fixed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,41 +3,43 @@ import Layout from "@/components/layout";
 import Blockpost from "@/components/common/blockpost";
 import { ToggleNav } from "@/components/layout/Navbar";
 import CreatePost from "@/components/home-page/createPost";
-import React , {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useDispatch, useSelector } from "react-redux";
-import { setQuestions, setToken } from "@/redux/slices/publicSlice";
-import { getCookie, hasCookie, setCookie } from "cookies-next";
-import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setQuestions } from "@/redux/slices/publicSlice";
+import { setCookie } from "cookies-next";
 
 const index = () => {
   const dispatch = useDispatch();
-  const publicSlice = useSelector((state) => state.publicData);
-
-  const token = getCookie('token');
   const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(true);    
+  const [isLoading, setLoading] = useState(true);
   const getAllClass = async () => {
     try {
-      setLoading(true)
-      const response = await fetch('/api/fetchAllQuestion/' + '643bfd04e7211dc61a4e23ba')
-      const json = await response.json()
-      setData(json.result.data)
-      console.log(json.result.data)
-    } catch(err) {
-      console.log(err)
+      setLoading(true);
+      const response = await fetch(
+        "/api/fetchAllQuestion/" + "643bfd04e7211dc61a4e23ba"
+      );
+      const json = await response.json();
+      setData(json.result.data);
+      console.log(json.result.data);
+    } catch (err) {
+      console.log(err);
     } finally {
-      console.log("get class data!")
-      setLoading(false)
+      console.log("get class data!");
+      setLoading(false);
     }
-  }
-  useEffect(() => {  
-
+  };
+  useEffect(() => {
     getAllClass();
   }, []);
 
-  if (!data) return <Layout><p className="text-white">No class data</p></Layout>
-  if (isLoading) return <p>Loading...</p>
+  if (!data)
+    return (
+      <Layout>
+        <p className="text-white text-center text-2xl">No class data</p>
+      </Layout>
+    );
+  if (isLoading) return <p>Loading...</p>;
   return (
     <Layout>
       <div className="m-8">
@@ -49,11 +51,27 @@ const index = () => {
           <div className="border-b border-636363 pb-5">
             <CreatePost />
           </div>
-          <div className="pt-5">{data.map((items) => (
-            <Link key={items.ID} href={"/home/question"} onClick={()=>{dispatch(setQuestions(items)), console.log(items), setCookie('questionId', items.ID)}}>
-              <Blockpost username = {items.username} dateTime = {items.created_at} titleQuestion = {items.Content} classTag = {items.classTag} comment = {items.answer === null ? '0' : items.answer.length}/>
-            </Link>
-          ))}</div>
+          <div className="pt-5">
+            {data.map((items) => (
+              <Link
+                key={items.ID}
+                href={"/home/question"}
+                onClick={() => {
+                  dispatch(setQuestions(items)),
+                    console.log(items),
+                    setCookie("questionId", items.ID);
+                }}
+              >
+                <Blockpost
+                  username={items.username}
+                  dateTime={items.created_at}
+                  titleQuestion={items.Content}
+                  classTag={items.classTag}
+                  comment={items.answer === null ? "0" : items.answer.length}
+                />
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/Home/question.js
+++ src/pages/Home/question.js
@@ -4,25 +4,25 @@ import Questionblock from "@/components/common/questionblock";
 import Typeanswer from "@/components/home-page/Typeanswer";
 import Answerblock from "@/components/home-page/Answerblock";
 import { useSelector } from "react-redux";
-import { getCookie, setCookie } from "cookies-next";
+import { getCookie } from "cookies-next";
 
 const question = () => {
   const publicSlice = useSelector((state) => state.publicData);
   const [data, setData] = useState([]);
   const getAnswer = async () => {
     try {
-      const questionId = getCookie('questionId')
-      const response = await fetch('/api/answerById/' + questionId)
-      const json = await response.json()
-      setData(json.result.data)
-      console.log(json.result.data)
-    } catch(err) {
-      console.log(err)
+      const questionId = getCookie("questionId");
+      const response = await fetch("/api/answerById/" + questionId);
+      const json = await response.json();
+      setData(json.result.data);
+      console.log(json.result.data);
+    } catch (err) {
+      console.log(err);
     } finally {
-      console.log("get answer data!")
+      console.log("get answer data!");
     }
-  }
-  useEffect(() => {  
+  };
+  useEffect(() => {
     getAnswer();
   }, []);
   return (
@@ -30,13 +30,23 @@ const question = () => {
       <Layout>
         <div className="m-8">
           <div className="m-auto max-w-xl lg:max-w-2xl 2xl:max-w-5xl">
-            <Questionblock username = {publicSlice.questions.username} dateTime = {publicSlice.questions.created_at} titleQuestion = {publicSlice.questions.Content} questionDetail = {""} classTag = {publicSlice.questions.classTagContent}/>
+            <Questionblock
+              username={publicSlice.questions.username}
+              dateTime={publicSlice.questions.created_at}
+              titleQuestion={publicSlice.questions.Content}
+              questionDetail={""}
+              classTag={publicSlice.questions.classTagContent}
+            />
             <div className="pt-5 w-full">
               <Typeanswer />
             </div>
             <div className="pt-5 m-auto w-full">
               {data.map((items) => (
-                <Answerblock username={items.username} dateTime={items.created_at} answer={items.content}/>
+                <Answerblock
+                  username={items.username}
+                  dateTime={items.created_at}
+                  answer={items.content}
+                />
               ))}
             </div>
           </div>
